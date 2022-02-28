ab65c6936bbe8abbf3ce80a6691e5eb2acbe8423
jakobhe <lewei@live.com>
2022-02-28 16:11:50 +0800

    checkstyle demo

--- src/main/java/com/jakobhe/demo/DemoApplication.java
+++ src/main/java/com/jakobhe/demo/DemoApplication.java
@@ -8,8 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args)  ;
-		// int abcdefghijklmnop8989qrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopq
-		// rstuvwxyzabcdefghijklmnopqrstuvwxyz = 888;
+		// int abcdefghijklmnop8989qrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz = 888;
 	}
 
 }
