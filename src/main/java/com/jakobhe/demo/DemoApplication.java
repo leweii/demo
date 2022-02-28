a1500b97b207540640516a8f9239d261b8239cc9
jakobhe <lewei@live.com>
2022-02-28 16:25:30 +0800

    checkstyle demo

--- src/main/java/com/jakobhe/demo/DemoApplication.java
+++ src/main/java/com/jakobhe/demo/DemoApplication.java
@@ -1,13 +1,15 @@
 package com.jakobhe.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.slf4j.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main (String[] args)  {
-		SpringApplication.run(DemoApplication.class, args)  ;
+		SpringApplication.run(DemoApplication.class, args)  ;   
+		int a=3;
 		//    int abcdefghijklmnop8989qrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopq
 		//    rstuvwxyzabcdefghijklmnopqrstuvwxyz = 888;
 	}
