749c46b90a117775ec447fdba4955f5c46c19691
jakobhe <lewei@live.com>
2022-02-28 16:12:29 +0800

    checkstyle demo

---
+++ src/main/java/com/jakobhe/demo/Test.java
@@ -0,0 +1,8 @@
+package com.jakobhe.demo;
+
+public class Test
+
+{
+    // int abcdefghijklmnop8989qrstuvwxyzabcdefghijklmnopqrstuvwxyzabcd
+    // efghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz = 888;
+}
