c81d7f98c984cf8475b08877282f0b8e954a5163
jakobhe <lewei@live.com>
2022-02-28 13:33:50 +0800

    checkstyle demo

---
+++ src/main/java/com/jakobhe/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.jakobhe.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		int abcdefghijklmnop8989qrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz = 888;
+	}
+
+}
