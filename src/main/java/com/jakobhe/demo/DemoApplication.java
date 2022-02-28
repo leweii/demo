ea539444f250cc1426408d12b365960c6f53e025
jakobhe <lewei@live.com>
2022-02-28 16:19:48 +0800

    checkstyle demo

--- src/main/java/com/jakobhe/demo/DemoApplication.java
+++ src/main/java/com/jakobhe/demo/DemoApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main (String[] args)  {
 		SpringApplication.run(DemoApplication.class, args)  ;
-		// int abcdefghijklmnop8989qrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopq
-		// rstuvwxyzabcdefghijklmnopqrstuvwxyz = 888;
+		//    int abcdefghijklmnop8989qrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopq
+		//    rstuvwxyzabcdefghijklmnopqrstuvwxyz = 888;
 	}
 
-}
+}   
